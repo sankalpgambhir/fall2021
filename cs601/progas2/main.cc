754c8aa79e2b4ffa5242144db5b1c5a3be34e9db
Sankalp Gambhir <sankalp.gambhir47@gmail.com>
2021-11-06 14:39:22 +0530

    assignment 2 framework

---
+++ cs601/progas2/main.cc
@@ -0,0 +1,24 @@
+/*
+    Game of Primes
+    CS 601 Programming Assignment 2
+    Sankalp Gambhir
+    180260032
+*/
+
+#include <iostream>
+#include <vector>
+
+bool check_strategy(std::vector<int> exponents, bool player = true);
+
+int main(){
+
+    // while input is being given, loop and check
+    while(false){
+        // take input
+
+        // call prime checker
+        std::cout << (int) check_strategy({1, 1, 1});
+    }
+
+    return 0;
+}
